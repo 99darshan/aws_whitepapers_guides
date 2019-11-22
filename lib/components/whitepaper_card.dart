87f93d3b9bcd5d0abcd35ae8b2cdaa682ae2b9c8
99darshan <dsn.adh@gmail.com>
2019-11-22 21:08:30 +0545

    added download pdf function to whitepaper card

--- lib/components/whitepaper_card.dart
+++ lib/components/whitepaper_card.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:io';
 import 'package:aws_whitepapers_guides/models/index.dart';
 import 'package:aws_whitepapers_guides/screens/pdf_view_screen.dart';
 import 'package:aws_whitepapers_guides/state/bookmark_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 class WhitepaperCard extends StatefulWidget {
@@ -108,7 +111,9 @@ class _WhitepaperCardState extends State<WhitepaperCard> {
                     IconButton(
                       icon: Icon(Icons.file_download,
                           color: Theme.of(context).primaryColor),
-                      onPressed: () {},
+                      onPressed: () {
+                        this._downloadWhitepaper(widget.whitepaperData);
+                      },
                     ),
                   ],
                 )
@@ -118,5 +123,34 @@ class _WhitepaperCardState extends State<WhitepaperCard> {
         ),
       ),
     );
+  }
+
+  // this download function reference will be passed to any view pdf screen as well
+  Function _downloadWhitepaper = (WhitepaperData whitepaper) async {
+    var taskId = await FlutterDownloader.enqueue(
+      url: whitepaper.item.additionalFields.primaryURL,
+      fileName: whitepaper.item.name,
+      savedDir: await _findLocalPath(),
+      showNotification: true,
+      openFileFromNotification: true,
+    );
+    //_showDownloadDialog(article);
+  };
+
+  // TODO: answer this https://stackoverflow.com/questions/51776109/how-to-get-the-absolute-path-to-the-download-folder/51777119
+  static Future<String> _findLocalPath() async {
+    // getExternalStorageDirectory() use this if to save to downloads dir
+    //final directory = await getApplicationDocumentsDirectory();
+    // getExternalStorageDirectory() gives /storage/emulated/0/Android/data/com.example.core_research/files
+    final directory = await getExternalStorageDirectory();
+    print(directory.path);
+    String path = directory.path +
+        '/Download'; // TODO: change directory folder name to AWSWhitepapers
+    final saveDir = Directory(path);
+    bool dirExists = await saveDir.exists();
+    if (!dirExists) {
+      saveDir.create();
+    }
+    return saveDir.path;
   }
 }
