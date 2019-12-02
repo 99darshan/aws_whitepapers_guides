3ba71d91fc5ce7bab0b05da1f63e880526bcc2bc
99darshan <dsn.adh@gmail.com>
2019-12-02 20:27:14 +0545

    modified search screen

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -20,53 +20,52 @@ class _SearchScreenState extends State<SearchScreen> {
     WhitepaperState whitepaperState = Provider.of<WhitepaperState>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search Whitepapers"),
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            decoration: InputDecoration(
+              focusColor: Colors.white,
+              enabledBorder: const OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.white,
+                ),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+              prefixIcon:
+                  Icon(Icons.search, color: Theme.of(context).primaryColor),
+            ),
+            onFieldSubmitted: (value) async {
+              print('searching...');
+              setState(() {
+                _searchText = value;
+              });
+              print('search text is $_searchText');
+              await whitepaperState
+                  .fetchWhitepapersBySearchKeywords(_searchText);
+              setState(() {
+                _isSearchingCompleted = true;
+              });
+            },
+          ),
+        ),
       ),
       body: Container(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(
-              height: 100,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                ),
-                onFieldSubmitted: (value) async {
-                  print('searching...');
-                  setState(() {
-                    _searchText = value;
-                  });
-                  print('search text is $_searchText');
-                  await whitepaperState
-                      .fetchWhitepapersBySearchKeywords(_searchText);
-                  setState(() {
-                    _isSearchingCompleted = true;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: whitepaperState.isFetchingData
-                  ? ShimmerList()
-                  : _isSearchingCompleted
-                      ? ListView.builder(
-                          shrinkWrap: false,
-                          itemCount:
-                              whitepaperState.searchAwsResponse.items.length,
-                          itemBuilder: (context, index) {
-                            return WhitepaperCard(
-                                whitepaperData: whitepaperState
-                                    .searchAwsResponse.items[index]);
-                          },
-                        )
-                      : SizedBox(width: 0.0),
-            )
-          ],
-        ),
+        color: Colors.grey[200],
+        //padding: EdgeInsets.all(8.0),
+        child: whitepaperState.isFetchingData
+            ? ShimmerList()
+            : _isSearchingCompleted
+                ? ListView.builder(
+                    shrinkWrap: false,
+                    itemCount: whitepaperState.searchAwsResponse.items.length,
+                    itemBuilder: (context, index) {
+                      return WhitepaperCard(
+                          whitepaperData:
+                              whitepaperState.searchAwsResponse.items[index]);
+                    },
+                  )
+                : SizedBox(width: 0.0),
       ),
     );
   }
