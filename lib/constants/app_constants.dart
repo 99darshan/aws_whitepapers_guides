14c2bdc7054a5230b3c4b7a64021f1fe35d93b2d
99darshan <dsn.adh@gmail.com>
2020-01-10 00:23:13 +0545

    updated banner ad unit ID with real ID

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,6 @@
 class AppConstants {
-  // static const BANNER_AD_UNIT_ID = "ca-app-pub-8453560050460431/3169528942";
+  static const BANNER_AD_UNIT_ID = "ca-app-pub-8453560050460431/3169528942";
+
   // TEST ID ca-app-pub-3940256099942544/6300978111
-  static const BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
+  // static const BANNER_AD_UNIT_ID = "ca-app-pub-3940256099942544/6300978111";
 }
